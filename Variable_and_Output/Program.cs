430c75bbc6fcb37dd88d72a9e815f275df5b6bd5
Kirgudu64 <pismo64@mail.ru>
2022-05-29 19:20:01 +0300

    Добавлена практическая работа: Реализация подсчёта количества баллов по всем предметам

--- Variable_and_Output/Program.cs
+++ Variable_and_Output/Program.cs
@@ -15,3 +15,17 @@ double ScoresPhysics = 93.5;                // Баллы по физике
 
 Console.WriteLine($"{"Фамилия Имя Отчество: ",30} {FullName,30} \n{"Возраст: ",30} {Age,30} \n{"Email: ",30} {Email,30} \n{"Баллы по программированию: ",30} {ScoresCoding,30} \n{"Баллы по математике: ",30} {ScoresMath,30} \n{"Баллы по физике: ",30} {ScoresPhysics,30}");
 Console.ReadKey();
+
+
+//
+// Практическая работа: Реализация подсчёта количества баллов по всем предметам
+//
+
+double sum = ScoresCoding + ScoresMath + ScoresPhysics;
+double average = sum / 3;
+
+string sumFormated = sum.ToString("### ###.##");
+string averageFormated = average.ToString("### ###.##");
+
+Console.WriteLine($"\n{"Сумма баллов: ",30} {sumFormated,30} \n{"Средний балл: ",30} {averageFormated,30}");
+Console.ReadKey();
